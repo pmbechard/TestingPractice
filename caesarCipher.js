ddaa08c8f6c4a52e7069036c9c603ccf6be6d95e
Peyton Bechard <pbechard@my.yorku.ca>
2022-06-17 12:07:11 +0800

    Adds test suite for Caesar cipher encoding function and implements functoin successfully

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,6 +1,41 @@
-class Cipher {
-  encode(text, shift) {}
-  decode(text, shift) {}
+export class Cipher {
+  static encode(text, shift) {
+    if (!this.#validateText(text) || !this.#validateShift(shift)) return '';
+    if (shift < 1) return text;
+    let result = [];
+    for (let i = 0; i < text.length; i++) {
+      let asciiVal = text.charCodeAt(i);
+      if (asciiVal >= 65 && asciiVal <= 90) {
+        let newVal = asciiVal + shift;
+        while (newVal > 90) {
+          newVal -= 26;
+        }
+        result.push(String.fromCharCode(newVal));
+      } else if (asciiVal >= 97 && asciiVal <= 122) {
+        let newVal = asciiVal + shift;
+        while (newVal > 122) {
+          newVal -= 26;
+        }
+        result.push(String.fromCharCode(newVal));
+      } else if (asciiVal >= 48 && asciiVal <= 57) {
+        let newVal = asciiVal + shift;
+        while (newVal > 57) {
+          newVal -= 10;
+        }
+        result.push(String.fromCharCode(newVal));
+      } else {
+        result.push(String.fromCharCode(asciiVal));
+      }
+    }
+    return result.join('');
+  }
+  static decode(text, shift) {
+    if (!this.#validateText(text) || !this.#validateShift(shift)) return '';
+  }
+  static #validateText(text) {
+    return typeof text === 'string';
+  }
+  static #validateShift(shift) {
+    return typeof shift === 'number' && !isNaN(shift);
+  }
 }
-
-export { Cipher };
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -7,26 +7,40 @@ test('Lowercase characters-only encoding', () => {
 
 test('Uppercase characters-only encoding', () => {
   expect(Cipher.encode('THIS IS A TEST', 3)).toBe('WKLV LV D WHVW');
-  expect(Cipher.encode('DICKY THE DOG', 2)).toBe('EJDLZ UIF EPH');
+  expect(Cipher.encode('DICKY THE DOG', 1)).toBe('EJDLZ UIF EPH');
 });
 
 test('Mixed-case characters-only encoding', () => {
   expect(Cipher.encode('Attack At Dawn', 5)).toBe('Fyyfhp Fy Ifbs');
-  expect(Cipher.encode('DiCkY tHe DoG', 2)).toBe('EjDlZ uIf EpH');
+  expect(Cipher.encode('DiCkY tHe DoG', 1)).toBe('EjDlZ uIf EpH');
 });
 
-test('Negative shift values for encoding', () => {
-  expect(Cipher.encode('attack at dawn', -7)).toBe('tmmtvd tm wtpg');
-  expect(Cipher.encode('DiCkY tHe DoG', -4)).toBe('ZeYgU pDa ZkC');
+test('Negative shift values for encoding return String as is', () => {
+  expect(Cipher.encode('attack at dawn', -7)).toBe('attack at dawn');
+  expect(Cipher.encode('DiCkY tHe DoG', -4)).toBe('DiCkY tHe DoG');
 });
 
-test('Invalid types for encoding and shift values return empty String', () => {
+test('Encoding strings including numbers', () => {
+  expect(Cipher.encode('123', 1)).toBe('234');
+  expect(Cipher.encode('059', 5)).toBe('504');
+  expect(Cipher.encode('a1b2c3', 2)).toBe('c3d4e5');
+});
+
+test('Encoding strings with non-letters and non-numbers', () => {
+  expect(Cipher.encode('hey!', 1)).toBe('ifz!');
+  expect(Cipher.encode('~!@#$%^&*()_+', 5)).toBe('~!@#$%^&*()_+');
+});
+
+test('Invalid types for encoding values return empty String', () => {
   expect(Cipher.encode(123, 1)).toBe('');
   expect(Cipher.encode([1, 2, 3], 2)).toBe('');
   expect(Cipher.encode({ key: 'value' }, 3)).toBe('');
   expect(Cipher.encode(undefined, 4)).toBe('');
   expect(Cipher.encode(NaN, 5)).toBe('');
   expect(Cipher.encode(null, 6)).toBe('');
+});
+
+test('Invalid types for shift values return empty String', () => {
   expect(Cipher.encode('valid', 'invalid')).toBe('');
   expect(Cipher.encode('valid', [1, 2, 3])).toBe('');
   expect(Cipher.encode('valid', { key: 'value' })).toBe('');
